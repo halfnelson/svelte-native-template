54899d99ca79b30bd4606e1ae49c23eb3182ed31
halfnelson <dpershouse@gmail.com>
2019-09-07 17:22:47 +1000

    Test tab impl

--- app/App.svelte
+++ app/App.svelte
@@ -1,19 +1,41 @@
 <page class="page">
-    <actionBar title="My App" icon="" class="action-bar">
-    </actionBar>
-    <stackLayout class="p-20">
-        <label text="Tap the button" class="h1 text-center"/>
-        <button text="TAP" on:tap="{ onTap }" class="btn btn-primary btn-active"/>
-        <label text="{ message }" class="h2 text-center" textWrap="true"/>
-    </stackLayout>
+        <actionBar title="test">
+          <stackLayout orientation="horizontal" horizontalAlignment="center">
+            <image src="res://icon" style="width: 30; height: 30; vertical-align: center; margin-right: 10;" />
+            <label style="font-size: 20; vertical-align: center;">
+              <formattedString>
+                <span text="Tabs" fontWeight="Bold" />
+                <span text="Navigation" />
+              </formattedString>
+            </label>
+          </stackLayout>
+        </actionBar>
+        <stackLayout>
+          <label text="{activeTab}" />
+          <button text="Click me!" on:tap="{ () => tabs = tabs.concat([{title: 'added', text: 'added tab'}]) }" />
+          <button text="Go to last!" on:tap="{ () => activeTab = tabs.length - 1 }" />
+          <gridLayout>
+            <tabs bind:selectedIndex="{activeTab}">
+              <tabStrip>
+               {#each tabs as tab,i ((i + tab.title))}
+                 <tabStripItem  title="{tab.title}"></tabStripItem>
+               {/each}
+              </tabStrip>
+              {#each tabs as tab,i ((i + tab.title))}
+                <tabContentItem>
+                    <label text="{tab.text}" />
+                </tabContentItem>
+              {/each}
+            </tabs>
+          </gridLayout>
+        </stackLayout>
 </page>
 
 <script>
-    let counter = 42;
-    let message;
-    $: message = (counter <= 0) 
-                    ? "Hoorraaay! You unlocked the Svelte-Native clicker achievement!"
-                    : `${counter} taps left`
-    
-    const onTap = () => counter--;
+    let activeTab = 1;
+    let tabs = [
+      { title: 'First Tab', text: 'im the first tab' },
+      { title: 'Second Tab', text: 'im the second tab' },
+      { title: 'Third Tab', text: 'im the third tab' }
+    ];
 </script>
