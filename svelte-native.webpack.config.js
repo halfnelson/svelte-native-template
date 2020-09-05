b18d639bfda11648d46073c25bb75cca82966943
halfnelson <dpershouse@gmail.com>
2020-09-05 20:58:33 +1000

    restore hmr support

--- svelte-native.webpack.config.js
+++ svelte-native.webpack.config.js
@@ -12,10 +12,11 @@ module.exports = env => {
             {
                 loader: 'svelte-loader-hot',
                 options: {
-                    dev: true,
+                    dev: env.production ? false : true,
                     preprocess: [preprocessConfig.preprocess, svelteNativePreprocessor()],
-                    hotReload: true,
+                    hotReload: env.production ? false : true,
                     hotOptions: {
+                        injectCss: false,
                         native: true
                     }
                 }
