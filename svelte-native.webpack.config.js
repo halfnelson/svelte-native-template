0cb78cc7f6d6b882e62dfc6d448828a7392991fc
korexus <christopher.fitzsimons@gmail.com>
2020-10-27 21:38:48 +0000

    Suppressing warnings raised by Svelte for A11y. They relate to HTML validation, not nativescript.

--- svelte-native.webpack.config.js
+++ svelte-native.webpack.config.js
@@ -18,6 +18,11 @@ module.exports = env => {
                     hotOptions: {
                         injectCss: false,
                         native: true
+                    },
+                    onwarn: (warning, onwarn) => {
+                        if (!/A11y:/.test(warning.message)) {
+                            onwarn(warning);
+                        }
                     }
                 }
             }
